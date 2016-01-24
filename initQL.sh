0bebf2b7e13c6c09c2990b00a22d0f777ff089f5
lurney <lurney@qq.com>
2016-01-24 14:42:27 +0800

    Update initQL.sh

--- initQL.sh
+++ initQL.sh
@@ -8,6 +8,9 @@ iptables -P FORWARD DROP
 iptables -A INPUT -i lo -p all -j ACCEPT 
 #开启SSH端口
 iptables -A INPUT -p tcp -m tcp --dport 16291 -j ACCEPT
+#允许DNS
+iptables -A INPUT -p tcp -m tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp -m udp --dport 53 -j ACCEPT
 #允许http和https
 iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
